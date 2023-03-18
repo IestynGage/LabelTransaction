3f9fc973ddcc329a586c41b58f8eb5d53e7f3038
Iestyn <>
2023-03-18 20:09:48 +0000

    Add tests

--- test/test_labeler.py
+++ test/test_labeler.py
@@ -1,8 +1,34 @@
 from unittest import TestCase
-from src.labeler import Labeler
+from src.labeler import Labeler, Label
 
-class TestIsValidLabel(TestCase):
-    def test_always_passes(self):
-        labelerObject = Labeler("test/resources/labels.json")
+class TestLabeler(TestCase):
+    def test_is_valid_label_true(self):
+        labelerObject = Labeler("test/resources/example-labels.json")
         result = labelerObject.isValidLabel("housing")
         self.assertTrue(result, "Expected label housing to be in labels.json")
+
+    def test_is_valid_label_false(self):
+        labelerObject = Labeler("test/resources/example-labels.json")
+        result = labelerObject.isValidLabel("blue")
+        self.assertFalse(result, "Expected label blue to not be valid")
+
+    def test_matches_label_true(self):
+        labelerObject = Labeler("test/resources/example-labels.json")
+        result = labelerObject.matchesLabel("rent")
+        self.assertEqual(result, "housing")
+
+    def test_matches_label_false(self):
+        labelerObject = Labeler("test/resources/example-labels.json")
+        result = labelerObject.matchesLabel("blue")
+        self.assertEqual(result, "")
+
+class TestLabel(TestCase):
+    def test_matches_true(self):
+        label = Label("Housing", ["rent", "internet"])
+        result = label.matchLabel("rent")
+        self.assertEqual(result, True)
+
+    def test_matches_false(self):
+        label = Label("Housing", ["rent", "internet"])
+        result = label.matchLabel("blue")
+        self.assertEqual(result, False)
