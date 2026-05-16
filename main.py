79b95553223b3c2b48f6255a9fd83f4450298fd3
Iestyn <33298011+IestynGage@users.noreply.github.com>
2026-05-16 12:32:09 +0100

    Update main to use new functios

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 from excel_connector import open_excel_file
 from exporter import export_transactions
-from parser import parse_csv_files
+from models import Transaction
+from parser import parse_csv_files, parse_excel
 
-from rules import Rules
+from rules import Rules, filter_by_month
 from tui.CSVCategorizer import CsvCategorizer
 from tui.ExtracMonth import MonthSelectorApp
+from tui.existing import continue_with_existing
 
 # TUIs to select remaining categories.
 
 # exporter
 
 def main():
-    # TODO Load current progress of excel. 
-    # Ask user continue loading excel thingy for month ... y/n
-    # Assuming the scenario you already ran this once this month
-    csv_files = CsvCategorizer().run()
-    month = MonthSelectorApp(csv_files).run()
-    transactions = parse_csv_files(csv_files)
+    existing_transactions:list[Transaction] = parse_excel('transactions.xlsx')
+
+    transactions:list[Transaction] = []
+    if continue_with_existing(existing_transactions):
+        transactions = existing_transactions
+    else:
+        csv_files = CsvCategorizer().run()
+        month = MonthSelectorApp(csv_files).run().split()[0]
+        print(month)
+        csv_transactions = parse_csv_files(csv_files)
+        print(len(csv_transactions))
+        transactions = filter_by_month(csv_transactions, month)
+        print(len(transactions))
+    
     labeled_transactions = Rules().apply(transactions)
     export_transactions(labeled_transactions)
     open_excel_file()
 
 if __name__ == "__main__":
     main()
+
+# Continue with existing t 
